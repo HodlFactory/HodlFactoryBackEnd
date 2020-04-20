fe00245a7c46a2d889a5cac046911bcb120cfd9e
Andrew Stanger <andrew@stanger.org.uk>
2020-04-20 12:59:21 +0100

    first draft ponzi

---
+++ contracts/PonziHodlFactory.sol
@@ -0,0 +1,166 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+/// @title Dai contract interface
+/// @notice Various cash functions
+interface Cash 
+{
+    function approve(address _spender, uint _amount) external returns (bool);
+    function balanceOf(address _ownesr) external view returns (uint);
+    function mint(uint256) external;
+    function transfer(address _to, uint _amount) external returns (bool);
+}
+
+interface IaToken 
+{
+    function balanceOf(address _user) external view returns(uint);
+    function redeem(uint256 _amount) external;
+}
+
+interface IAaveLendingPool 
+{
+    function deposit( address _reserve, uint256 _amount, uint16 _referralCode) external;
+}
+
+interface IAaveLendingPoolCore {}
+
+contract PonziHodlFactory is ERC721Full {
+
+    using SafeMath for uint;
+
+    Cash underlying;
+    IaToken aToken;
+    IAaveLendingPool aaveLendingPool;
+    IAaveLendingPoolCore aaveLendingPoolCore;
+
+    constructor(address _cashAddress, address _aTokenAddress, address _aaveLpAddress, address _aaveLpcoreAddress ) ERC721Full("HodlFactory", "HODL++") public { 
+        underlying = Cash(_cashAddress);
+        aToken = IaToken(_aTokenAddress); 
+        aaveLendingPool = IAaveLendingPool(_aaveLpAddress); 
+        aaveLendingPoolCore = IAaveLendingPoolCore(_aaveLpcoreAddress); 
+
+        //create first tier
+        tierProperties[0].size = 10;
+    }
+
+    uint public hodlCount = 0;
+    uint public tierCount = 0;
+    uint public averagePurchaseTime = 0;
+    uint constant public oneHundredDai = 10**20;
+    uint public testingVariableA = 69;
+    uint public testingVariableB = 0;
+    uint public testingVariableC = 0;
+    uint public testingVariableD = 0;
+    uint public testingVariableE = 0;
+
+    struct hodl {
+        uint purchaseTime;
+        uint tier;
+    }
+
+    struct tier {
+        uint averagePurchaseTime;
+        uint interestAlreadyWithdrawn;
+        uint hodlsInTier;
+        uint size;
+    }
+
+    mapping (uint => hodl) public hodlProperties; 
+    mapping (uint => tier) public tierProperties; 
+    mapping (address => uint[]) hodlOwnerTracker;
+
+    event stfu(uint indexed stfu);
+
+    function getHodlPurchaseTime(uint _hodlId) external view returns (uint) {
+        return hodlProperties[_hodlId].purchaseTime;
+    }
+
+    function getAdaiBalance() public view returns (uint) {
+        return(aToken.balanceOf(address(this)));
+    }
+
+    function getHodlsOwned() public view returns(uint[] memory) {
+        return(hodlOwnerTracker[msg.sender]);
+    }
+
+    function _createNewTier() internal {
+        uint _sizeOfBiggestTier = tierProperties[tierCount].size;
+        uint _newSize = (_sizeOfBiggestTier.mul(10)).div(9);
+        tierCount = tierCount.add(1);
+        tierProperties[tierCount].size = _newSize;
+    }
+
+    function addToTier(uint _hodlId) public {
+        //add to existing tier
+        uint _hodlsInTier = tierProperties[tierCount].hodlsInTier;
+        uint _sizeOfTier = tierProperties[tierCount].size;
+        uint _tierAveragePurchaseTime = tierProperties[tierCount].averagePurchaseTime;
+        if (_hodlsInTier < _sizeOfTier) {
+            tierProperties[tierCount].hodlsInTier = _hodlsInTier.add(1);
+            tierProperties[tierCount].averagePurchaseTime = ((_tierAveragePurchaseTime.mul(_hodlsInTier)).add(now)).div(_hodlsInTier.add(1));
+        } else {
+            _createNewTier();
+            addToTier(_hodlId);
+        }
+    }
+
+    function createHodl() public {
+        // UPDATE VARIABLES
+        hodlOwnerTracker[msg.sender].push(hodlCount);
+        hodlProperties[hodlCount].purchaseTime = now;
+        hodlProperties[hodlCount].tier = tierCount;
+        averagePurchaseTime = ((averagePurchaseTime.mul(hodlCount)).add(now)).div(hodlCount.add(1));
+        addToTier(hodlCount);
+        // SWAP DAI FOR aDAI
+        underlying.mint(oneHundredDai);
+        underlying.approve(address(aaveLendingPoolCore), oneHundredDai);
+        aaveLendingPool.deposit(address(underlying), oneHundredDai, 0);
+        // // GENERATE NFT
+        _mint(msg.sender, hodlCount);
+        hodlCount = hodlCount.add(1);  
+    } 
+
+    function getTierInterestAvailableToWithdraw(uint _tierId) public view returns (uint) {
+        uint _totalAdaiBalance = aToken.balanceOf(address(this)); 
+        uint _totalDaiBalance = hodlCount.mul(oneHundredDai);
+        uint _totalInterestAvailable = _totalAdaiBalance.sub(_totalDaiBalance);
+        uint _hodlsInTier = tierProperties[_tierId].hodlsInTier;
+        uint _interestAlreadyWithdrawn = tierProperties[_tierId].interestAlreadyWithdrawn;
+        uint _numerator = _totalInterestAvailable.mul(now.sub(tierProperties[_tierId].averagePurchaseTime)).mul(_hodlsInTier);
+        uint _denominator = (now.sub(averagePurchaseTime)).mul(hodlCount);
+        return ((_numerator.div(_denominator)).sub(_interestAlreadyWithdrawn));
+    }
+
+    // function withdrawInterest(uint _hodlId) public {
+    //     uint _interestToWithdraw = getInterestAvailableToWithdraw(_hodlId);
+    //     // update variables
+    //     uint _sumOfLastWithdrawTimes = averageTimeLastWithdrawn.mul(hodlCount);
+    //     uint _sumOfLastWithdrawTimesUpdated = _sumOfLastWithdrawTimes.add(now).sub(hodlProperties[_hodlId].interestLastWithdrawnTime);
+    //     averageTimeLastWithdrawn = _sumOfLastWithdrawTimesUpdated.div(hodlCount);
+    //     hodlProperties[_hodlId].interestLastWithdrawnTime = now;
+    //     // external calls
+    //     aToken.redeem(_interestToWithdraw);
+    //     underlying.transfer(ownerOf(_hodlId), _interestToWithdraw);
+    // }
+
+    // function destroyHodl(uint _hodlId) public {
+    //     require (ownerOf(_hodlId) == msg.sender, "Not owner");
+    //     // require (hodlProperties[_hodlId].purchaseTime.add(3600) < now, "HODL must be owned for 1 hour");
+    //     withdrawInterest(_hodlId);
+    //     // update averageTimeLastWithdrawn
+    //     if (hodlCount > 1) {
+    //         averageTimeLastWithdrawn = ((averageTimeLastWithdrawn.mul(hodlCount)).sub(hodlProperties[_hodlId].interestLastWithdrawnTime)).div(hodlCount.sub(1));
+    //     } else {
+    //         averageTimeLastWithdrawn = 0;
+    //     }
+    //     // external calls
+    //     aToken.redeem(oneHundredDai);
+    //     underlying.transfer(ownerOf(_hodlId), oneHundredDai);
+    //     // remove HODL
+    //     hodlCount = hodlCount.sub(1);
+    //     _burn(_hodlId);
+    // }
+
+}
